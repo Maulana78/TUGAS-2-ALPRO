f3b6c163618497c331458b316672b6dd2571a723
Maulana78 <61927822+Maulana78@users.noreply.github.com>
2020-03-29 18:45:31 +0700

    Add files via upload

---
+++ tugas 2 (no 5).cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void abc(char *a)
+{
+	int i;
+	for(i=0;i<15;i++)
+	{
+		if(a[i]>=65&&a[i]<=90)
+		{    
+		    a[i]=a[i]+32;
+		}
+		else if(a[i]>97&&a[i]<=122)
+	    {
+	    	a[i]=a[i]-32;
+		}
+	}
+	   
+}
+
+int main()
+{
+	  char a[15];
+	  printf("konversi huruf kecil ke huruf besar dan sebaliknya\nInput 1 kata: ");
+	  scanf("%s",a);
+	  abc(a);
+	  printf("Hasil: %s\n",a);
+	  return 0;
+}
